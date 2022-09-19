c20b72f428a34cee54bb4d0e455ec4c5ab28c6bb
Cameron <Alleyria@gmail.com>
2022-09-19 10:57:56 +0200

    lecture content

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :chef_name, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, shallow: true, only: %i[new create destroy]
+
+    collection do
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+  end
 end
